a32f0011804ebbcff50e226358b3f1344adfd950
Sebastian Scheid <s.scheid@cern.ch>
2022-01-11 13:16:45 +0100

    initial commit sebastian

---
+++ PlottingFW/src/user/sebastian.cpp
@@ -0,0 +1,112 @@
+// c++ includes
+#include <iostream>
+#include <vector>
+using namespace std;
+// ROOT includes
+#include "TString.h"
+#include "TMath.h"
+#include "TF2.h"
+#include "TCanvas.h"
+#include "TFile.h"
+#include "TList.h"
+#include "TStyle.h"
+#include "TBenchmark.h"
+#include "TLegend.h"
+
+#include "TH1.h"
+#include "TH2.h"
+#include "TPad.h"
+#include "TF1.h"
+#include "TLine.h"
+#include "TPaveText.h"
+#include "TVirtualFitter.h"
+#include "TGraph.h"
+#include "TMinuit.h"
+#include "TColor.h"
+
+// project includes
+// core
+#include "../core/LmManager.h"
+#include "../core/LmFileManager.h"
+#include "../core/LmBaseSignal.h"
+#include "../core/LmSignal.h"
+#include "../core/LmHandler.h"
+#include "../core/LmHelper.h"
+#include "../core/LmQA.h"
+#include "../core/LmMultiQA.h"
+#include "../core/LmFitter.h"
+#include "../core/LmBackground.h"
+// user
+#include "sebastian.h"
+
+// Start of analysis
+
+int main(int argc, char const* argv[])
+{
+  LmHelper lmHelper; // for styles etc.
+  lmHelper.SetDebugLvl(debug);
+
+  LmManager* mgr = new LmManager(const_cast<char*>(configString[0].Data()));
+  // set all kinds of configuration for the manager
+  mgr->SetInputhistAndDim(const_cast<char*>(inputhist.Data()), inputdim);
+  mgr->SetProjOption(projoption);
+  mgr->SetEnableEffiCorr(kFALSE);
+
+  //mgr00->SetEnableRebinning(kFALSE); // use this to disable or just comment out the setter above...
+  // configure 3D exclusion cut
+
+  // process the manager
+  // if (kDoEffiCorrectionFirst) {
+  //   mgr->SetDoEffiCorr();
+  // }
+  mgr->SetRebinning2D(vMeeBinsDef.size(), vMeeBinsDef.data(), vPteeBinsDef.size(), vPteeBinsDef.data()); // Check which binning to use...
+  mgr->SetEtaRange(-0.8, 0.8);
+  mgr->SetPtRange(kSetMinPtLabel, kSetMaxPtLabel);
+  mgr->Process();
+
+  // create a background object
+  LmBackground* bkg = new LmBackground(*(mgr));
+  bkg->SetUseRfactor(kDoRfactor);
+  bkg->SetRfactorUnityRange(0.5);
+  bkg->Process(LmBackground::kHybrid);
+
+  LmSignal* sig = new LmSignal(*(mgr), *(bkg));
+  //sig->SetDoSubtraction(kFALSE);
+  sig->SetDoSignifRaw(kDoSignifRaw);
+  sig->SetDoNormBinwidth(kDoNormBinwidth);
+  sig->SetDoNormNevents(kDoNormNevents);
+  // sig->SetCrossSection(kCrossSection); // for pp
+  // sig->SetDoNormCrossSection(kDoNormCrossSection); // plot as cross section, for pp
+  // sig->SetTrgEff(kTriggerEfficiency);
+  // sig->SetDoNormTrgEff(kDoNormTrgEff);
+  // sig->SetDoNormRecEff(kDoNormRecEff);
+  // sig->SetVtxRecEff(kVtxRecEff);
+  // sig->SetDoNormVtxRecEff(kDoNormVtxRecEff);
+  // sig->SetVtxRecEffee(kVtxRecEffee);
+  // sig->SetDoNormVtxRecEffee(kDoNormVtxRecEffee);
+  sig->Process(LmSignal::kStd);
+
+  std::vector<double> binsPtee_forMee = {0.,10};
+  std::vector<double> binsMee_forPtee = {0.14,0.7,1.1,3.1,8.};
+  double plotMeeMin = 0.;
+  double plotPteeMin= 0.;
+  double plotMeeMax = 5.;
+  double plotPteeMax= 8.;
+
+  LmHandler* hanCompare = new LmHandler(binsPtee_forMee, binsMee_forPtee, plotMeeMin, plotMeeMax, plotPteeMin, plotPteeMax);
+
+  hanCompare->AttachSignal(*(sig));
+  hanCompare->Process(LmHandler::kStd);
+
+  for (int i = 0; i < binsPtee_forMee.size(); i++) {
+    /* code */
+    hanCompare->PrintCompareSoverB_Mee(i,Form("soverb_mee_%d.pdf",i));
+    hanCompare->PrintCompareSignif_Mee(i,Form("signif_mee_%d.pdf",i));
+  }
+  for (int i = 0; i < binsMee_forPtee.size(); i++) {
+    hanCompare->PrintCompareSoverB_Ptee(i,Form("soverb_ptee_%d.pdf",i));
+    hanCompare->PrintCompareSignif_Ptee(i,Form("signif_ptee_%d.pdf",i));
+  }
+
+  return 0;
+}
