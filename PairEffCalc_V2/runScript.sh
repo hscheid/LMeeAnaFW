da658ad47d67d7f49976928dfde2f13452d0cc04
Sebastian Scheid <s.scheid@cern.ch>
2022-02-03 11:35:20 +0100

    Lazy...

---
+++ PairEffCalc_V2/runScript.sh
@@ -0,0 +1,7 @@
+./calcEffi ./configs/config-LHC21b6b.txt
+./calcEffi ./configs/config-LHC21b6.txt
+./CalcEffi ./configs/config-LHC19h9.txt
+./calcEffi ./configs/config-LHC20f11c.txt
+./calcEffi ./configs/config-LHC20f11c_oob.txt
+./calcEffi ./configs/config-LHC21b6b_oob.txt
+./calcEffi ./configs/config-LHC21b6_oob.txt
